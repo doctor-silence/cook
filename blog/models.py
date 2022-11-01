a53d4c4b4797bbb497558c0da40da73c56965272
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-11-01 22:07:32 +0700

    add_model

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,26 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    parent = models.ForeignKey(
-        'self',
-        related_name='children',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
+    parent = TreeForeignKey('self', related_name='children', on_delete=models.SET_NULL, null=True, blank=True)
 
+    class MPTTMeta:
+        order_insertion_by = ['name']
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='articles/')
+    text = models.TextField()
+    category = models.ForeignKey(Category, related_name='post', on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag, related_name='post')
+    create_at = models.DateTimeField(auto_now_add=True)
+
 
 
