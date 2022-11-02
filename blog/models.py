1c39bbf01834d9de4784f4db663b6f91cb7a6dab
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-11-02 19:08:20 +0700

    add_new_model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
@@ -15,12 +16,34 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='articles/')
     text = models.TextField()
     category = models.ForeignKey(Category, related_name='post', on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag, related_name='post')
     create_at = models.DateTimeField(auto_now_add=True)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    serves = models.CharField(max_length=50)
+    prep_time = models.PositiveIntegerField(default=0)
+    cook_time = models.PositiveIntegerField(default=0)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    post = models.ForeignKey(Post, related_name='recipe', on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    website = models.CharField(max_length=150)
+    message = models.TextField(max_length=500)
+    post = models.ForeignKey(Post, related_name='comment', on_delete=models.CASCADE)
+
+
+
 
 
 
