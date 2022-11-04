8f3787186b59d3778d3142b56599c385a413ab14
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-11-04 18:19:56 +0700

    add_admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,21 @@ from mptt.admin import MPTTModelAdmin
 
 from . import models
 
+
+class RecipeUnLine(admin.StackedInline):
+    model = models.Recipe
+    extra = 1
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'create_at', 'id']
+    inlines = [RecipeUnLine]
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'prep_time', 'cook_time', 'post']
+
 admin.site.register(models.Category, MPTTModelAdmin)
 admin.site.register(models.Tag)
-admin.site.register(models.Post)
-admin.site.register(models.Recipe)
 admin.site.register(models.Comment)
 
