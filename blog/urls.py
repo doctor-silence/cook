30c2a636f741583667c2fc937c62610466bff90d
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-11-11 18:44:33 +0700

    add_url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name="post_single"),
     path('<slug:slug>/', views.PostListView.as_view(), name='post_list'),
     path('', views.home),
